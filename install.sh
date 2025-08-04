badb9fa3bd24f3bd373bd53ad98ff3c58f271865
AlterStrong <kucing.lucuuuu12@gmail.com>
2025-08-04 15:33:17 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+cd $HOME
+rm -rf roblox_monitor
+mkdir -p roblox_monitor
+cd roblox_monitor
+
+curl -LO https://raw.githubusercontent.com/AlterStrong/Roblox-BSS-launcher/main/roblox_monitor.sh
+chmod +x roblox_monitor.sh
+
+echo ""
+echo "✅ Skrip berhasil diunduh."
+echo "Jalankan perintah berikut:"
+echo "bash roblox_monitor.sh setup"
