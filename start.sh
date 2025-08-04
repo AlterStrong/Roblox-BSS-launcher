4d515d48116de83590c18f5ed886cf62ca1f27c3
AlterStrong <kucing.lucuuuu12@gmail.com>
2025-08-04 16:21:23 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 cd "$(dirname "$0")"
-
 FILE="roblox_monitor.sh"
 URL="https://raw.githubusercontent.com/AlterStrong/Roblox-BSS-launcher/main/$FILE"
 
-# Download jika file belum ada
 if [ ! -f "$FILE" ]; then
   echo "[INFO] Mengunduh $FILE..."
   curl -s -O "$URL" || { echo "Gagal mengunduh $FILE"; exit 1; }
   chmod +x "$FILE"
 fi
 
-# Jalankan skrip
 bash "$FILE" start
