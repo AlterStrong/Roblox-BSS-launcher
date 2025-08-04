87d590ee7e31dffcc0b46b68ecd6be7d202f8441
AlterStrong <kucing.lucuuuu12@gmail.com>
2025-08-04 16:36:29 +0700

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,10 +1,2 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
-cd "$(dirname "$0")"
-FILE="roblox_monitor.sh"
-
-if [ -f "$FILE" ]; then
-  bash "$FILE" stop
-else
-  echo "[ERROR] File roblox_monitor.sh tidak ditemukan!"
-fi
+bash ~/roblox_monitor.sh stop
