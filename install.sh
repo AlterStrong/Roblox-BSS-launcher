769a3ee7faddc572a8c8887a7dbe953a41387caf
AlterStrong <kucing.lucuuuu12@gmail.com>
2025-08-04 16:47:14 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
-# === Setup awal ===
-pkg update -y
-pkg install -y curl termux-api
-
-cd ~
-rm -f roblox_monitor.sh start.sh stop.sh
-
-# Unduh ulang semua file dari GitHub
-curl -Lo roblox_monitor.sh https://raw.githubusercontent.com/AlterStrong/Roblox-BSS-launcher/main/roblox_monitor.sh
-curl -Lo start.sh https://raw.githubusercontent.com/AlterStrong/Roblox-BSS-launcher/main/start.sh
-curl -Lo stop.sh https://raw.githubusercontent.com/AlterStrong/Roblox-BSS-launcher/main/stop.sh
-
-chmod +x roblox_monitor.sh start.sh stop.sh
-
-echo "✅ Instalasi selesai. Jalankan: bash roblox_monitor.sh setup"
+cd $HOME
+rm -rf Roblox-BSS-launcher
+git clone https://github.com/AlterStrong/Roblox-BSS-launcher
+cd Roblox-BSS-launcher
+cp roblox_monitor.sh start.sh stop.sh ~
+chmod +x ~/roblox_monitor.sh ~/start.sh ~/stop.sh
+echo "✅ Instalasi selesai. Jalankan:"
+echo "bash roblox_monitor.sh setup"
